b0380649861121fe87f91e4be57156d50fb8ba46
Bhavani-20 <iamdurgabhavani20@gmail.com>
2024-11-08 11:54:45 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -21,7 +21,7 @@ public class HelloController {
         }
 
         // Return the greeting message with the IPv4 address
-        return "Greetings from  'vignan' deployed JAVA app in virtual Machine..!!! Host private IPv4 Address: " + ipAddress;
+        return "Greetings from  'DURGA BHAVANI' deployed JAVA app in Kubernets..!!! Host private IPv4 Address: " + ipAddress;
     }
     
 }
